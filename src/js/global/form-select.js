549f133cf452cad07c6d32174f2ebba20c17eaad
Ksenia Balashova <utyatka@gmail.com>
2020-08-19 15:44:59 +0200

    add main layout

---
+++ src/js/global/form-select.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// инициализация селект2
+(function ($) {
+  $(document).ready(function () {
+    $(".custom-select").select2({
+      width: "200px",
+    });
+  });
+})(window.jQuery);
