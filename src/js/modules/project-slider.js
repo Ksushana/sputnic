f3dd66d6be314dd163c8da3ed5d1adcf45cdddca
Ksenia Balashova <utyatka@gmail.com>
2020-08-20 14:16:50 +0200

    wip

---
+++ src/js/modules/project-slider.js
@@ -0,0 +1,12 @@
+(function () {
+  var mySwiper = new Swiper('.project__slider', {
+    speed: 400,
+    spaceBetween: 100,
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+})();
