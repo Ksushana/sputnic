53e7da17049203a08ec8ecdfae9c24c2ebd434de
Ksenia Balashova <utyatka@gmail.com>
2020-08-21 02:18:45 +0200

    wip

--- build/js/script.js
+++ build/js/script.js
@@ -61,6 +61,31 @@ n+=b;l.push(b);d.on("keydown keypress keyup input paste propertychange",{c:d,ind
   };
 })();
 
+(() => {
+  let inputs = document.querySelectorAll(`input`);
+
+  for (let input of inputs) {
+    if (input.value.length) {
+      input.classList.add(`filled`);
+    }
+  }
+
+  const toggleInput = (evt) => {
+    let input = evt.target;
+
+    if (input.classList && input.classList.contains(`form__input`)) {
+      if (input.value.length) {
+        input.classList.add(`filled`);
+      } else {
+        input.classList.remove(`filled`);
+      }
+    }
+  };
+
+  document.addEventListener(`blur`, toggleInput, true);
+  document.addEventListener(`input`, toggleInput, true);
+})();
+
 (function () {
   var mySwiper = new Swiper('.product__slider', {
     speed: 400,
---
+++ src/js/modules/form-label.js
@@ -0,0 +1,24 @@
+(() => {
+  let inputs = document.querySelectorAll(`input`);
+
+  for (let input of inputs) {
+    if (input.value.length) {
+      input.classList.add(`filled`);
+    }
+  }
+
+  const toggleInput = (evt) => {
+    let input = evt.target;
+
+    if (input.classList && input.classList.contains(`form__input`)) {
+      if (input.value.length) {
+        input.classList.add(`filled`);
+      } else {
+        input.classList.remove(`filled`);
+      }
+    }
+  };
+
+  document.addEventListener(`blur`, toggleInput, true);
+  document.addEventListener(`input`, toggleInput, true);
+})();
